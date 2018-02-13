dca723dda3b9f4bef80a98ef936885cbd455069f
Jose Alan <alan123art@gmail.com>
2018-02-13 17:39:44 -0300

    4.2 - Reconhecendo mensagem de aluno e trocando som da mensagem

--- app/src/main/java/br/com/alura/agenda/receiver/SMSReceiver.java
+++ app/src/main/java/br/com/alura/agenda/receiver/SMSReceiver.java
@@ -3,15 +3,38 @@ package br.com.alura.agenda.receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.telephony.SmsMessage;
 import android.widget.Toast;
+
+import br.com.alura.agenda.R;
+import br.com.alura.agenda.dao.AlunoDAO;
 
 /**
  * Created by Alan on 11/02/2018.
  */
 
 public class SMSReceiver extends BroadcastReceiver {
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     public void onReceive(Context context, Intent intent) {
-        Toast.makeText(context, "Chegou um SMS!", Toast.LENGTH_SHORT).show();
+
+        Object[] pdus = (Object[]) intent.getSerializableExtra("pdus");
+        byte[] pdu = (byte[]) pdus[0];
+        String formato = (String) intent.getSerializableExtra("format");
+
+        SmsMessage sms = SmsMessage.createFromPdu(pdu,formato);
+
+        String telefone = sms.getDisplayOriginatingAddress();
+        AlunoDAO dao = new AlunoDAO(context);
+        if(dao.ahAluno(telefone)){
+            Toast.makeText(context, "Chegou um SMS de Aluno!", Toast.LENGTH_SHORT).show();
+            MediaPlayer mp = MediaPlayer.create(context, R.raw.msg);
+            mp.start();
+        }
+        dao.close();
+
     }
 }
